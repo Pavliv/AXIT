a8ff61d21bd49828a3667ac2fa938330bfeb6307
Pavliv <maryan.pavliv@gmail.com>
2017-03-14 15:10:31 +0200

    add JavaScript and references

---
+++ scripts/app.js
@@ -0,0 +1,21 @@
+function showTab1 () {
+	document.getElementById('change_tab').innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis dis parturient montes.<br><br> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euis mod bibendum laoreet.";
+	document.getElementById('change_tab').style.fontSize = "14px";
+	document.getElementById('tab_1').style.backgroundColor = "#ff8b38";
+	document.getElementById('tab_2').style.backgroundColor = "#333";
+	document.getElementById('tab_3').style.backgroundColor = "#333";
+}
+function showTab2 () {
+	document.getElementById('change_tab').innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euis mod bibendum laoreet.";
+	document.getElementById('change_tab').style.fontSize = "14px";
+	document.getElementById('tab_2').style.backgroundColor = "#ff8b38";
+	document.getElementById('tab_1').style.backgroundColor = "#333";
+	document.getElementById('tab_3').style.backgroundColor = "#333";
+}
+function showTab3 () {
+	document.getElementById('change_tab').innerHTML = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euis mod bibendum laoreet.";
+	document.getElementById('change_tab').style.fontSize = "25px";
+	document.getElementById('tab_3').style.backgroundColor = "#ff8b38";
+	document.getElementById('tab_1').style.backgroundColor = "#333";
+	document.getElementById('tab_2').style.backgroundColor = "#333";
+}
